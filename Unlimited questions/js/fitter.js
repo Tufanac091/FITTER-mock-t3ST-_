b32098f66cdfb53fb1674b3c3ecfaa31b9b9f69c
Th3__R()HiiT__iiNSiiD3 :) <tufanac098@gmail.com>
2025-07-18 20:28:51 +0530

    Create fitter.js

---
+++ Unlimited questions/js/fitter.js
@@ -0,0 +1,52 @@
+const questions = [
+  {
+    question: "फाइलिंग करते समय कौन-सा टूल काम आता है?\nWhich tool is used during filing?",
+    options: ["हैमर / Hammer", "फाइल / File", "स्क्रू ड्राइवर / Screwdriver", "वर्नियर / Vernier"],
+    answer: 1
+  },
+  {
+    question: "वर्नियर कैलीपर किस लिए प्रयोग होता है?\nWhat is a Vernier Caliper used for?",
+    options: ["गहराई मापने / Depth", "मोटाई मापने / Thickness", "लंबाई मापने / Length", "ऊंचाई मापने / Height"],
+    answer: 0
+  },
+  {
+    question: "लैथ मशीन का प्रयोग किस लिए होता है?\nLathe machine is used for?",
+    options: ["कटिंग / Cutting", "टर्निंग / Turning", "बोरिंग / Boring", "सभी / All of these"],
+    answer: 3
+  },
+  {
+    question: "टैपिंग किस प्रक्रिया से जुड़ी है?\nTapping is related to?",
+    options: ["ड्रिलिंग / Drilling", "थ्रेडिंग / Threading", "ग्राइंडिंग / Grinding", "कटिंग / Cutting"],
+    answer: 1
+  },
+  {
+    question: "एंगल प्लेट का उपयोग होता है?\nAngle plate is used for?",
+    options: ["मापने / Measuring", "सपोर्ट देने / Supporting", "काटने / Cutting", "पॉलिशिंग / Polishing"],
+    answer: 1
+  },
+  {
+    question: "हैकसॉ का उपयोग होता है?\nHacksaw is used for?",
+    options: ["फाइलिंग / Filing", "ड्रिलिंग / Drilling", "काटने / Cutting", "घिसाई / Grinding"],
+    answer: 2
+  },
+  {
+    question: "ड्रिल बिट के कोण को क्या कहते हैं?\nAngle of drill bit is called?",
+    options: ["लीड / Lead", "प्वाइंट एंगल / Point angle", "हेलिक्स / Helix", "चम्फर / Chamfer"],
+    answer: 1
+  },
+  {
+    question: "रेमर का प्रयोग होता है?\nReamer is used in?",
+    options: ["स्लॉटिंग / Slotting", "ड्रिलिंग / Drilling", "होल फिनिशिंग / Hole finishing", "ग्राइंडिंग / Grinding"],
+    answer: 2
+  },
+  {
+    question: "वर्कपीस को पकड़ने के लिए वाइस कहाँ लगाई जाती है?\nWhere is the vice fixed to hold workpiece?",
+    options: ["टेबल / Table", "वर्कबेंच / Workbench", "दीवार / Wall", "ग्राउंड / Ground"],
+    answer: 1
+  },
+  {
+    question: "पिच गॉज़ का उपयोग होता है?\nPitch gauge is used for?",
+    options: ["थ्रेड चेक करने / Thread check", "लेंथ नापने / Measuring length", "होल चेक / Hole check", "वेट नापने / Measuring weight"],
+    answer: 0
+  }
+];
